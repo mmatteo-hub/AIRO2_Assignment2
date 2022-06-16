24000e612a191b716c54f6e2978bf5a1d4023d1f
Mattia Musumeci <mattia.musumeci@yahoo.it>
2022-06-16 17:11:00 +0200

    run file

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 POPF=./popf-tif/planner/release/popf/popf3-clp
+
+echo "The default popf-tif intallation folder is ./popf-tif ."
+echo "Do you want to a custom popf-tif installation? (y/n)"
+read ANS
+
+if [[ $ANS == 'y' ]]
+then
+    echo "Insert the custom popf-tif installation folder."
+    read POPF
+fi
+
 DOMAIN=./assignment_files/visits_domain
 EXTERNAL=./assignment_files/visits_module/build
 
