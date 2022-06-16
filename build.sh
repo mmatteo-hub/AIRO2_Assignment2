b877d5f92ac09a6dd83e853b64581c62cce48488
mmatteo-hub <4636216@studenti.unige.it>
2022-06-16 15:12:03 +0200

    fixed

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -r ./assignment_files/visits_module/build
+mkdir ./assignment_files/visits_module/build
+
 cd ./assignment_files/visits_module/build
 cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_BUILD_TYPE=Release ../src/
 make
