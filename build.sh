174563856a054fa836dfa68f277d990391d2e955
Mattia Musumeci <mattia.musumeci@yahoo.it>
2022-06-16 17:03:20 +0200

    build file

--- build.sh
+++ build.sh
@@ -1,8 +1,58 @@
-#!/bin/sh
+#!/bin/bash
+# Storing the main project location
+MAIN_DIR=$(pwd)
 
-rm -r ./assignment_files/visits_module/build
-mkdir ./assignment_files/visits_module/build
+# Asking the user if he wants to download popf from the repo
+echo "The official popf-tif repo is (https://github.com/popftif/popf-tif)."
+echo "Do you want to download it from the official repo? (y/n)"
+echo "NB. This will remove all the files in ./popf-tif ."
+read ANS
 
-cd ./assignment_files/visits_module/build
-cmake -DCMAKE_VERBOSE_MAKEFILE=TRUE -DCMAKE_BUILD_TYPE=Release ../src/
-make
+if [[ $ANS == 'y' ]]
+then
+    # Removes any existing file and recreates the directory
+    rm -f -r ./popf-tif
+    mkdir -p ./popf-tif
+    # Clones the repository
+    cd ./popf-tif
+    git clone https://github.com/popftif/popf-tif .
+    
+    echo "Do you want to build popf-tif? (y/n)"
+    read ANS
+    
+    if [[ $ANS == 'y' ]]
+    then
+        # Removes any existing build files
+        cd ./planner
+        rm -f -r ./debug
+        rm -f -r ./release
+        # Automatically builds the planner
+        cd ./src
+        chmod +x ./build-instructions.txt
+        ./build-instructions.txt 
+        cd ../release
+        make popf3-clp
+    fi 
+fi
+
+# Returning to the main project directory
+cd "$MAIN_DIR"
+
+echo "Do you want to build the external module? (y/n)"
+read ANS
+
+if [[ $ANS == 'y' ]]
+then
+    # Removes any existing build files
+    rm -f -r ./assignment_files/visits_module/build
+    # Builds the external module
+    cd ./assignment_files/visits_module/src
+    chmod +x ./buildInstruction.txt
+    ./buildInstruction.txt
+fi
+
+# Returning to the main project directory
+cd "$MAIN_DIR"
+
+exit
+
--- install_popf-tif.sh
+++
@@ -1,6 +0,0 @@
-cd popf-tif
-git clone https://github.com/popftif/popf-tif .
-cd planner/src
-./build-instructions.txt 
-cd ../release
-make popf3-clp 
